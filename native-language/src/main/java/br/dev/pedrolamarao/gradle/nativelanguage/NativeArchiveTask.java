a79ff833691fd2b33bc256d1102bf73792d5ff5c
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-09-18 20:03:16 -0300

    Add NativeArchiveTask.

--- native-language/src/main/java/br/dev/pedrolamarao/gradle/nativelanguage/NativeArchiveTask.java
+++ native-language/src/main/java/br/dev/pedrolamarao/gradle/nativelanguage/NativeArchiveTask.java
@@ -3,38 +3,35 @@
 package br.dev.pedrolamarao.gradle.nativelanguage;
 
 import org.gradle.api.file.RegularFileProperty;
-import org.gradle.api.provider.ListProperty;
-import org.gradle.api.tasks.*;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.SourceTask;
+import org.gradle.api.tasks.TaskAction;
 import org.gradle.process.ExecOperations;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
 
-public abstract class NativeLinkTask extends SourceTask
+public abstract class NativeArchiveTask extends SourceTask
 {
     final ExecOperations execOperations;
-
-    @Input
-    public abstract ListProperty<String> getOptions ();
 
     @OutputFile
     public abstract RegularFileProperty getOutput ();
 
     @Inject
-    public NativeLinkTask (ExecOperations execOperations)
+    public NativeArchiveTask (ExecOperations execOperations)
     {
         this.execOperations = execOperations;
     }
 
     @TaskAction
-    public void link ()
+    public void archive ()
     {
         final var target = getOutput().getAsFile().get().toPath();
 
         final var command = new ArrayList<String>();
-        command.add("clang");
-        command.addAll(getOptions().get());
-        command.add("-o");
+        command.add("llvm-ar");
+        command.add("rcs");
         command.add(target.toString());
         getSource().forEach(file -> command.add(file.toString()));
 

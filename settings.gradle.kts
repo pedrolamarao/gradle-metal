f8727be216e8bd7e1395dd10431bbfef3f68843f
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-09-28 23:59:53 -0300

    Declare dependendency repository.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 // Copyright (c) Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "gradle-metal"
 
 include("plugins")

8066d01ed9e7378b2ff698f089be94ff50a48d97
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-10-01 12:26:56 -0300

    Apply plugin-publish.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 // Copyright (c) Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
 
 plugins {
+    id("com.palantir.git-version")
     id("base")
 }
 
+val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+
 group = "br.dev.pedrolamarao.gradle.metal"
-version = "1.0-SNAPSHOT"
+version = versionDetails().let { "0.1+${it.gitHash}" }
 
 subprojects {
     group = rootProject.group
--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -1,52 +1,83 @@
 // Copyright (c) Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
 
 plugins {
-    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish")
     id("jvm-test-suite")
-    id("maven-publish")
 }
 
 gradlePlugin {
+    website = "https://github.com/pedrolamarao/gradle-metal"
+    vcsUrl = "https://github.com/pedrolamarao/gradle-metal.git"
     plugins {
         create("application") {
             id = "br.dev.pedrolamarao.metal.application"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.application.MetalApplicationPlugin"
+            displayName = "gradle-metal application component plugin"
+            description = "Configures a conventional gradle-metal application component"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("archive") {
             id = "br.dev.pedrolamarao.metal.archive"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.archive.MetalArchivePlugin"
+            displayName = "gradle-metal archive component plugin"
+            description = "Configures a conventional gradle-metal archive component"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("asm") {
             id = "br.dev.pedrolamarao.metal.asm"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.asm.MetalAsmPlugin"
+            displayName = "gradle-metal assembler language plugin"
+            description = "Contributes assembler language support to gradle-metal components"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("base") {
             id = "br.dev.pedrolamarao.metal.base"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.base.MetalBasePlugin"
+            displayName = "gradle-metal base plugin"
+            description = "Contributes native tasks and configurations"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("c") {
             id = "br.dev.pedrolamarao.metal.c"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.c.MetalCPlugin"
+            displayName = "gradle-metal c language plugin"
+            description = "Contributes C language support to gradle-metal components"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("commands") {
             id = "br.dev.pedrolamarao.metal.commands"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.commands.MetalCommandsPlugin"
+            displayName = "gradle-metal commands database plugin"
+            description = "Contributes a task to aggregate compile_commands.json database"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("cpp") {
             id = "br.dev.pedrolamarao.metal.cpp"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.cpp.MetalCppPlugin"
+            displayName = "gradle-metal cpp support plugin"
+            description = "Contributes C preprocessor support to gradle-metal components"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("cxx") {
             id = "br.dev.pedrolamarao.metal.cxx"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.cxx.MetalCxxPlugin"
+            displayName = "gradle-metal cxx language plugin"
+            description = "Contributes C++ language support to gradle-metal components"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("prebuilt") {
             id = "br.dev.pedrolamarao.metal.prebuilt"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.prebuilt.MetalPrebuiltPlugin"
+            displayName = "gradle-metal prebuilt plugin"
+            description = "Permits defining a prebuilt gradle-metal component"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
         create("ixx") {
             id = "br.dev.pedrolamarao.metal.ixx"
             implementationClass = "br.dev.pedrolamarao.gradle.metal.ixx.MetalIxxPlugin"
+            displayName = "gradle-metal ixx plugin"
+            description = "Contributes C++ module interface language support to gradle-metal components"
+            tags = setOf("bare-metal","asm","c","cpp","cxx","c++","native")
         }
     }
 }

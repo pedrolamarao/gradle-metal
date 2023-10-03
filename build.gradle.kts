769d6110fd4902c4b8b0c1a9d20b91c3383ba267
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-10-03 13:33:48 -0300

    Update version to 0.1-rc-0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 // Copyright (c) Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
 
 plugins {
-    id("com.palantir.git-version")
     id("base")
 }
 
-val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
-
 group = "br.dev.pedrolamarao.gradle.metal"
-version = versionDetails().let { "0.1+${it.gitHash}" }
+version = "0.1-rc-0"
 
 subprojects {
     group = rootProject.group

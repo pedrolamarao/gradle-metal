54d07f3f16105d40e21a078a830e0c7fcb542442
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-11-29 19:57:17 -0300

    Always build development samples with current code.

--- samples/dependency-on-gradle-build/settings.gradle.kts
+++ samples/dependency-on-gradle-build/settings.gradle.kts
@@ -1,15 +1,5 @@
 pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("br.dev.pedrolamarao.metal.")) {
-                useModule("br.dev.pedrolamarao.gradle.metal:plugins:0.5+")
-            }
-        }
-    }
+    includeBuild("../..")
 }
 
 includeBuild("application")

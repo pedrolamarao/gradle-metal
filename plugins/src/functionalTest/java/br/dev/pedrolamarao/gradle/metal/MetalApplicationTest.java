5c006e4eabfea7a64984186c341b68d284e51f36
Pedro Lamarão <pedro.lamarao@gmail.com>
2024-02-21 12:20:01 -0300

    Add tests for the desired DSL.

--- plugins/src/functionalTest/java/br/dev/pedrolamarao/gradle/metal/MetalApplicationTest.java
+++ plugins/src/functionalTest/java/br/dev/pedrolamarao/gradle/metal/MetalApplicationTest.java
@@ -95,6 +95,96 @@ class MetalApplicationTest extends MetalTestBase
         }
     }
 
+    @DisplayName("compile with filtered Assembler sources")
+    @Test
+    void compileFilteredAsm () throws IOException
+    {
+        Files.createDirectories(projectDir.resolve("src/main/asm"));
+
+        Files.writeString(projectDir.resolve("src/main/asm/main.s"),
+            """
+            .global main
+            main:
+                ret
+            """
+        );
+
+        Files.writeString(projectDir.resolve("src/main/asm/oops.s"),
+            """
+            this obviously cannot compile
+            """
+        );
+
+        Files.writeString(projectDir.resolve("build.gradle.kts"),
+            """
+            plugins {
+                id("br.dev.pedrolamarao.metal.application")
+                id("br.dev.pedrolamarao.metal.asm")
+            }
+            
+            application {
+                include("main.*")
+            }
+            """
+        );
+
+        final var compile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileAsm")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( compile.task(":compileAsm").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/obj/main/asm")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var recompile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileAsm")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( recompile.task(":compileAsm").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        final var link = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","link")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( link.task(":link").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/exe/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var relink = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","link")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( relink.task(":link").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        try (var stream = Files.walk(projectDir.resolve("build/exe/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+    }
+
     @DisplayName("compile with C sources")
     @Test
     void compileC () throws IOException
@@ -133,6 +223,116 @@ class MetalApplicationTest extends MetalTestBase
             plugins {
                 id("br.dev.pedrolamarao.metal.application")
                 id("br.dev.pedrolamarao.metal.c")
+            }
+            """
+        );
+
+        final var compile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileC")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( compile.task(":compileC").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/obj/main/c")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(2);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var recompile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileC")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( recompile.task(":compileC").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        final var link = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","link")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( link.task(":link").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/exe/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var relink = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","link")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( relink.task(":link").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        try (var stream = Files.walk(projectDir.resolve("build/exe/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+    }
+
+    @DisplayName("compile with filtered C sources")
+    @Test
+    void compileFilteredC () throws IOException
+    {
+        Files.createDirectories(projectDir.resolve("src/main/cpp"));
+        Files.writeString(projectDir.resolve("src/main/cpp/foo.h"),
+            """
+            int foo ();
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/c"));
+        Files.writeString(projectDir.resolve("src/main/c/foo.c"),
+            """
+            #include <foo.h>
+            
+            int foo ()
+            {
+                return 0;
+            }
+            """
+        );
+
+        Files.writeString(projectDir.resolve("src/main/c/main.c"),
+            """
+            #include <foo.h>
+            
+            int main (int argc, char * argv[])
+            {
+                return foo();
+            }
+            """
+        );
+
+        Files.writeString(projectDir.resolve("src/main/c/oops.c"),
+            """
+            this obviously cannot compile
+            """
+        );
+
+        Files.writeString(projectDir.resolve("build.gradle.kts"),
+            """
+            plugins {
+                id("br.dev.pedrolamarao.metal.application")
+                id("br.dev.pedrolamarao.metal.c")
+            }
+            
+            application {
+                exclude("oops.*")
             }
             """
         );
@@ -249,6 +449,137 @@ class MetalApplicationTest extends MetalTestBase
             
             application {
                 compileOptions = listOf("-std=c++20")
+            }
+            """
+        );
+
+        final var compile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","--info","compileCxx")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( compile.task(":compileCxx").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/obj/main/cxx")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(3);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var recompile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileCxx")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( recompile.task(":compileCxx").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        final var link = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","link")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( link.task(":link").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/exe/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var relink = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","link")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( relink.task(":link").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        try (var stream = Files.walk(projectDir.resolve("build/exe/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+    }
+
+    @DisplayName("compile with filtered C++ sources")
+    @Test
+    void compileFilteredCxx () throws IOException
+    {
+        Files.createDirectories(projectDir.resolve("src/main/cpp"));
+        Files.writeString(projectDir.resolve("src/main/cpp/foo.h"),
+            """
+            int foo ();
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/ixx"));
+        Files.writeString(projectDir.resolve("src/main/ixx/bar.ixx"),
+            """
+            module;
+            
+            #include <foo.h>
+            
+            export module bar;
+            
+            export int bar () { return foo(); }
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/ixx"));
+        Files.writeString(projectDir.resolve("src/main/ixx/oops.ixx"),
+            """
+            this obviously cannot compile
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/cxx"));
+        Files.writeString(projectDir.resolve("src/main/cxx/foo.cxx"),
+            """
+            #include <foo.h>
+            
+            int foo ()
+            {
+                return 0;
+            }
+            """
+        );
+
+        Files.writeString(projectDir.resolve("src/main/cxx/main.cxx"),
+            """
+            import bar;
+            
+            int main (int argc, char * argv[])
+            {
+                return bar();
+            }
+            """
+        );
+
+        Files.writeString(projectDir.resolve("src/main/cxx/oops.cxx"),
+            """
+            this obviously cannot compile
+            """
+        );
+
+        Files.writeString(projectDir.resolve("build.gradle.kts"),
+            """
+            plugins {
+                id("br.dev.pedrolamarao.metal.application")
+                id("br.dev.pedrolamarao.metal.cxx")
+            }
+            
+            application {
+                compileOptions = listOf("-std=c++20")
+                exclude("oops.cxx")
             }
             """
         );
--- plugins/src/functionalTest/java/br/dev/pedrolamarao/gradle/metal/MetalLibraryTest.java
+++ plugins/src/functionalTest/java/br/dev/pedrolamarao/gradle/metal/MetalLibraryTest.java
@@ -115,6 +115,117 @@ class MetalLibraryTest extends MetalTestBase
         assertThat( check.task(":check").getOutcome() ).isEqualTo(SUCCESS);
     }
 
+    @DisplayName("compile with filtered Assembler sources")
+    @Test
+    void compileFilteredAsm () throws IOException
+    {
+        Files.createDirectories(projectDir.resolve("src/main/asm"));
+        Files.writeString(projectDir.resolve("src/main/asm/foo.s"),
+            """
+            .intel_syntax
+            .global foo
+            foo:
+                xor eax, eax
+                ret
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/test/asm"));
+        Files.writeString(projectDir.resolve("src/test/asm/main.s"),
+            """
+            .intel_syntax
+            .global main
+            main:
+                call foo
+                ret
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/test/asm"));
+        Files.writeString(projectDir.resolve("src/test/asm/oops.s"),
+            """
+            this obviously cannot compile
+            """
+        );
+
+        Files.writeString(projectDir.resolve("build.gradle.kts"),
+            """
+            plugins {
+                id("br.dev.pedrolamarao.metal.library")
+                id("br.dev.pedrolamarao.metal.asm")
+            }
+            
+            library {
+                include("main.*")
+            }
+            """
+        );
+
+        final var compile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileAsm")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( compile.task(":compileAsm").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/obj/main/asm")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var recompile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileAsm")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( recompile.task(":compileAsm").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        final var archive = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","archive")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( archive.task(":archive").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/lib/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var rearchive = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","archive")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( rearchive.task(":archive").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        try (var stream = Files.walk(projectDir.resolve("build/lib/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        final var check = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","--info","check")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( check.task(":check").getOutcome() ).isEqualTo(SUCCESS);
+    }
+
     @DisplayName("compile with C sources")
     @Test
     void compileC () throws IOException
@@ -155,6 +266,126 @@ class MetalLibraryTest extends MetalTestBase
             plugins {
                 id("br.dev.pedrolamarao.metal.library")
                 id("br.dev.pedrolamarao.metal.c")
+            }
+            """
+        );
+
+        final var compile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileC")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( compile.task(":compileC").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/obj/main/c")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var recompile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileC")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( recompile.task(":compileC").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        final var archive = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","archive")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( archive.task(":archive").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/lib/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var rearchive = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","archive")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( rearchive.task(":archive").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        try (var stream = Files.walk(projectDir.resolve("build/lib/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        final var check = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","--info","check")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( check.task(":check").getOutcome() ).isEqualTo(SUCCESS);
+    }
+
+    @DisplayName("compile with C sources")
+    @Test
+    void compileFilteredC () throws IOException
+    {
+        Files.createDirectories(projectDir.resolve("src/main/cpp"));
+        Files.writeString(projectDir.resolve("src/main/cpp/foo.h"),
+            """
+            int foo ();
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/c"));
+        Files.writeString(projectDir.resolve("src/main/c/foo.c"),
+            """
+            #include <foo.h>
+            
+            int foo ()
+            {
+                return 0;
+            }
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/test/c"));
+        Files.writeString(projectDir.resolve("src/test/c/main.c"),
+            """
+            #include <foo.h>
+            
+            int main (int argc, char *  argv[])
+            {
+                return foo();
+            }
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/test/c"));
+        Files.writeString(projectDir.resolve("src/test/c/oops.c"),
+            """
+            this obviously cannot compile
+            """
+        );
+
+        Files.writeString(projectDir.resolve("build.gradle.kts"),
+            """
+            plugins {
+                id("br.dev.pedrolamarao.metal.library")
+                id("br.dev.pedrolamarao.metal.c")
+            }
+            
+            library {
+                exclude("oops.*")
             }
             """
         );
@@ -283,6 +514,162 @@ class MetalLibraryTest extends MetalTestBase
             }
             """
         );
+        Files.writeString(projectDir.resolve("build.gradle.kts"),
+            """         
+            plugins {
+                id("br.dev.pedrolamarao.metal.library")
+                id("br.dev.pedrolamarao.metal.cxx")
+            }
+            
+            library {
+                compileOptions = listOf("-std=c++20")
+            }
+            """
+        );
+
+        final var compile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileCxx")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( compile.task(":compileCxx").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/bmi/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        try (var stream = Files.walk(projectDir.resolve("build/obj/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(3);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var recompile = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","compileCxx")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( recompile.task(":compileCxx").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        final var archive = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","archive")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( archive.task(":archive").getOutcome() ).isEqualTo(SUCCESS);
+
+        try (var stream = Files.walk(projectDir.resolve("build/lib/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","clean")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        final var rearchive = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","archive")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( rearchive.task(":archive").getOutcome() ).isEqualTo(FROM_CACHE);
+
+        try (var stream = Files.walk(projectDir.resolve("build/lib/main")).filter(Files::isRegularFile)) {
+            assertThat( stream.count() ).isEqualTo(1);
+        }
+
+        final var check = GradleRunner.create()
+            .withArguments("--build-cache","--configuration-cache","--info","check")
+            .withPluginClasspath()
+            .withProjectDir(projectDir.toFile())
+            .build();
+
+        assertThat( check.task(":check").getOutcome() ).isEqualTo(SUCCESS);
+    }
+
+    @DisplayName("compile with C++ sources")
+    @Test
+    void compileFilteredCxx () throws IOException
+    {
+        Files.createDirectories(projectDir.resolve("src/main/cpp"));
+        Files.writeString(projectDir.resolve("src/main/cpp/foo.h"),
+            """
+            int foo ();
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/ixx"));
+        Files.writeString(projectDir.resolve("src/main/ixx/bar.ixx"),
+            """            
+            export module bar;
+            
+            export int bar ();
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/ixx"));
+        Files.writeString(projectDir.resolve("src/main/ixx/oops.ixx"),
+            """            
+            this obviously cannot compile
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/main/cxx"));
+        Files.writeString(projectDir.resolve("src/main/cxx/foo.cxx"),
+            """
+            #include <foo.h>
+            
+            int foo ()
+            {
+                return 0;
+            }
+            """
+        );
+        Files.writeString(projectDir.resolve("src/main/cxx/bar.cxx"),
+            """
+            module;
+            
+            #include <foo.h>
+            
+            module bar;
+            
+            int bar ()
+            {
+                return foo();
+            }
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/test/cxx"));
+        Files.writeString(projectDir.resolve("src/test/cxx/main.cxx"),
+            """
+            #include <foo.h>
+            
+            import bar;
+            
+            int main (int argc, char * argv[])
+            {
+                return foo() + bar();
+            }
+            """
+        );
+
+        Files.createDirectories(projectDir.resolve("src/test/cxx"));
+        Files.writeString(projectDir.resolve("src/test/cxx/oops.cxx"),
+            """
+            this obviously cannot compile
+            """
+        );
+
         Files.writeString(projectDir.resolve("build.gradle.kts"),
             """         
             plugins {

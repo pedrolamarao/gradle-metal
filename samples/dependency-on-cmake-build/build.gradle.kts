d894670027d8b80418147186c49393883611d351
Pedro Lamarão <pedro.lamarao@gmail.com>
2024-02-21 10:43:24 -0300

    Add commands dependencies.

--- samples/dependency-on-cmake-build/build.gradle.kts
+++ samples/dependency-on-cmake-build/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     id("br.dev.pedrolamarao.metal.base")
 }
+
+dependencies {
+    commands(project(":application"))
+}
--- samples/dependency-on-gradle-build/build.gradle.kts
+++ samples/dependency-on-gradle-build/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("br.dev.pedrolamarao.metal.base")
 }
 
+dependencies {
+    commands("br.dev.pedrolamarao.gradle.metal.sample:application:1.0")
+    commands("br.dev.pedrolamarao.gradle.metal.sample:archive:1.0")
+}
+
 tasks.build.configure {
     dependsOn( gradle.includedBuild("application").task(":build") )
 }
